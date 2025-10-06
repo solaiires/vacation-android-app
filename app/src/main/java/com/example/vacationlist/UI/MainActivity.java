1620982588e5f60490d6155f25e10672a05c5918
syette <124036055+solaiires@users.noreply.github.com>
2025-10-05 18:16:02 -0700

    added second screen vacationlist.

--- app/src/main/java/com/example/vacationlist/UI/MainActivity.java
+++ app/src/main/java/com/example/vacationlist/UI/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.vacationlist.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,VacationList.class);
+                intent.putExtra("test", "Information sent");
+                startActivity(intent);
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,5 +37,3 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
-
-// test
--- app/src/main/java/com/example/vacationlist/UI/VacationList.java
+++ app/src/main/java/com/example/vacationlist/UI/VacationList.java
@@ -17,6 +17,8 @@ public class VacationList extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_vacation_list);
+
+        System.out.println(getIntent().getStringExtra("test"));
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
